d179c6d972bb53db04ed46b446cc84af1f4d7058
hellobugme <1037714455@qq.com>
2015-10-16 02:59:57 +0800

    Update Steps.js

--- Steps.js
+++ Steps.js
@@ -7,7 +7,7 @@
 "use strict";
 function Steps(){
     if(!(this instanceof Steps)) return new Steps([].slice.call(arguments, 0));
-    this.fns = [];
+    this.fns = [], this.args = [];
     arguments.length > 0 && this.then.apply(this, arguments[0].push ? arguments[0] : [].slice.call(arguments, 0));
 }
 Steps.prototype = {
@@ -22,15 +22,14 @@ Steps.prototype = {
                 for(var args = [], i = 0, l = _this.step.length; i < l; i++) if(_this.step[i].done) return;
                 for(i = 0, l = _this.args.length; i < l; i++) args = args.concat(_this.args[i]);
                 _this.done.apply(_this, args);
+                _this.args = [];
             }
         }
         this.fns.push(fns);
         return this;
     },
     done : function(){
-        this.args = [];
-        var fns = this.step = this.fns.shift(),
-            args = [].slice.call(arguments, 0);
+        var fns = this.step = this.fns.shift(), args = [].slice.call(arguments, 0);
         if(fns) for(var i = 0, l = fns.length; i < l; i++) fns[i].apply(fns[i], args);
     }
 };
