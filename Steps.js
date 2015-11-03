31e781d5b76106775754895d092ea5d05dc3a995
hellobugme <1037714455@qq.com>
2015-11-03 23:44:14 +0800

    Update Steps.js

--- Steps.js
+++ Steps.js
@@ -15,6 +15,7 @@ Steps.prototype = {
     constructor : Steps,
     then : function(){
         var _this = this, fns = arguments[0].push ? arguments[0] : [].slice.call(arguments, 0);
+        if(fns.length === 0) return this;
         for(var i = 0, l = fns.length; i < l; i++){
             fns[i].index = i;
             fns[i].done = function(){
